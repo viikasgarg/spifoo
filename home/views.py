7b08907ddd1efd9c56f0a822ae3f6d6cbcc3ef9c
vikas kumar <viikasgarg@gmail.com>
2016-12-05 18:48:07 +0530

    template done

--- home/views.py
+++ home/views.py
@@ -5,8 +5,11 @@ from django.conf import settings
 
 def home(request, *args, **kwargs):
     url = request.GET.get('url', '')
-    url = url.split("//")[-1].split("/")[0]
-    site_type = settings.DATA.get(url, '')
+    site_type = ''
+    _url = url.split("//")[-1].split("/")[0]
+    if url:
+        site_type = settings.DATA.get(_url, "unknown")
+
     return render(request, "home.html",
                   {'url': url,
                    'site_type': site_type
