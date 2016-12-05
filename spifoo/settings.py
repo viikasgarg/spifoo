1092a89cb29a53d8a8a4a6d6b614898cd74e565e
vikas kumar <viikasgarg@gmail.com>
2016-12-05 13:29:33 +0530

    Initial Release 3

--- spifoo/settings.py
+++ spifoo/settings.py
@@ -100,3 +100,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+STATIC_ROOT = 'staticfiles/'
