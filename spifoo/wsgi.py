edb30ebcfaa5c8ba35c567451144398347d51ee8
vikas kumar <viikasgarg@gmail.com>
2016-12-05 13:26:22 +0530

    Initial Release 1

--- spifoo/wsgi.py
+++ spifoo/wsgi.py
@@ -1,16 +1,21 @@
 """
-WSGI config for spifoo project.
+WSGI config for indiapost project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
+
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spifoo.settings")
 
-application = get_wsgi_application()
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
